37109b3a757cbaeeb98ec7ed08a4c927b05e7084
Dipan <dipanmaulick25@gmail.com>
2026-01-16 05:53:01 +0000

    share icon added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Button } from './components/ui/Button'
 import { PlusIcon } from './icons/PlusIcon'
+import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
   return (
     <>
-      <Button variant="primary" text="Share" />
+      <Button startIcon={<ShareIcon size={"md"}/>} variant="primary" text="Share" />
       <Button  startIcon={<PlusIcon size={'md'}/>} variant="secondary" text="Add Content"/>
     </>
   )
