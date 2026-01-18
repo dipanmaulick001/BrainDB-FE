ea05fe481e3a9f3d4a143155ede27a82725c67cc
Dipan <dipanmaulick25@gmail.com>
2026-01-18 15:00:17 +0000

    BrainDB logo added & hovers

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../../icons/Logo"
 import { TwitterIcon } from "../../icons/TwitterIcon"
 import { YoutubeIcon } from "../../icons/YoutubeIcon"
 import {SidebarItem} from "./SidebarItem"
@@ -5,7 +6,10 @@ import {SidebarItem} from "./SidebarItem"
 
 export const Sidebar = ()=>{
     return <div className="h-screen bg-white border-r fixed w-64 left-0 top-0">
-        <div className="flex text-2xl pt-4 pl-2">
+        <div className="flex text-2xl pt-4">
+            <div className="pr-4 pl-2 pt-1.5">
+                <Logo/>
+            </div>
             BrainDB
         </div>
         <div className="pt-4">
